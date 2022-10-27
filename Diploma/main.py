68d3927987612bca30ea39c6e5ba759eeeb9fccc
Nikolay Malyshev <malyshev.na@phystech.edu>
2022-10-27 18:39:08 +0300

    Commit

--- Diploma/main.py
+++ Diploma/main.py
@@ -1,1 +1,9 @@
-# Абсолютно новый коммит
+import sys
+
+line_arr = []
+for line in sys.stdin:
+    line_arr.append([x.strip() for x in line.split('/')])
+
+new_line_arr = filter(lambda x: x[1] == line_arr[-1][0], line_arr[:-1])
+for line in sorted(new_line_arr, key=lambda x: -x[0]):
+    print(line[0])
