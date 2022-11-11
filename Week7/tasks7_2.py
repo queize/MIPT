1388fa8b53967fb986004680f1dad091ece017b4
Nikolay Malyshev <malyshev.na@phystech.edu>
2022-11-11 16:30:57 +0300

    Commit

--- Week7/tasks7_2.py
+++ Week7/tasks7_2.py
@@ -1,5 +1,7 @@
 class Nums:
-
-    arr: list
-
-        def __init__(self, x):
+    def __init__(self, u0, u1, p, q, n) -> None:
+        self.p = p
+        self.q = q
+        self.n = n
+        self.u0 = u0
+        self.u1 = u1
